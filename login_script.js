0d982e3999416e13e43d4bcf4e0079745fc26fcd
IKRafa <rafakadek14@gmail.com>
2026-01-09 19:27:43 +0800

    Update login_script.js

--- login_script.js
+++ login_script.js
@@ -1,13 +1,1 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-if (registerBtn && loginBtn && container) {
-    registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-    });
-
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-    });
-}
+const container=document.getElementById('container');const registerBtn=document.getElementById('register');const loginBtn=document.getElementById('login');if(registerBtn&&loginBtn&&container){registerBtn.addEventListener('click',()=>{container.classList.add("active")});loginBtn.addEventListener('click',()=>{container.classList.remove("active")})}
